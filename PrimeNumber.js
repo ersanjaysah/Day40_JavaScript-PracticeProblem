09bf68e7ee808d82c34e23241082a62688fff8f9
sanjay <ssah6694@gmail.com>
2022-04-24 17:29:05 +0530

    [sanjay]Added: Prime number file

---
+++ PrimeNumber.js
@@ -0,0 +1,18 @@
+var num =prompt("Please enter a number to check for prime number: ");
+
+var count=0;
+for (var i=1;i<=num;i++)
+{
+    if (num%i==0)
+    {
+        count=count+1;
+    }
+}
+
+if (count==2){
+    console.log("Entered number is Prime Number");
+}
+else
+{
+    console.log("Enter number is not a prime number")
+}
